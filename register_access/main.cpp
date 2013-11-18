c019304a9155b94ac9365bf1fdb659cdbc374005
mbodmer <mb0dm3r@gmail.com>
2013-11-18 23:35:21 +0100

    Initial Import register_access

---
+++ register_access/main.cpp
@@ -0,0 +1,11 @@
+// http://barendgehrels.blogspot.ch/2010/10/tag-dispatching-by-type-tag-dispatching.html
+
+#include <iostream>
+
+// http://accu.org/index.php/journals/1606
+
+int main() {
+
+    return 0;
+}
+

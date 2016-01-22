9cba832f5a09be4234f83c549288348fcccb9451
Marc Bodmer <marc.bodmer@securiton.ch>
2016-01-22 16:12:35 +0100

    Added specialisation for other message in directives example

--- directives/directives.cpp
+++ directives/directives.cpp
@@ -12,12 +12,20 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 
 struct Message {
   std::string destination;
   std::string source;
   uint64_t    id;
   Message() : destination("empty"), source("empty"), id(0ULL) {}
+};
+
+struct OtherMessage {
+  std::string destination;
+  std::string source;
+  std::string otherId;
+  OtherMessage() : destination("U"), source("V"), otherId("W") {}
 };
 
 // In cpp-netlib the directives are simple function objects that take a target
@@ -49,9 +57,27 @@ struct id_directive {
     const uint64_t& value;
     explicit id_directive(const uint64_t& id) : value(id) {}
     id_directive(const id_directive& other) : value(other.value) {}
+
+    template <class Input> Input& operator()(Input& input) const;
+};
+template<> Message& id_directive::operator()<Message>(Message& input) const {
+    // do something to Input
+    input.id = value;
+    return input;
+};
+template<> OtherMessage& id_directive::operator()<OtherMessage>(OtherMessage& input) const {
+    std::ostringstream o;
+    o << value;
+    input.otherId = o.str();
+    return input;
+};
+
+struct to_defaults_directive {
+    explicit to_defaults_directive() {}
     template <class Input> Input& operator()(Input& input) const {
-        // do something to Input
-        input.id = value;
+        input.source = "A";
+        input.destination = "B";
+        input.id = 11;
         return input;
     }
 };
@@ -70,6 +96,10 @@ inline const id_directive id(const uint64_t& value) {
     return id_directive(value);
 }
 
+inline const to_defaults_directive to_defaults() {
+    return to_defaults_directive();
+}
+
 
 // The trivial implementation of the directive protocol then boils down to the
 // specialization of the shift-left operator on the target type.
@@ -78,6 +108,10 @@ inline Message& operator<<(Message& msg, const Directive& directive) {
   return directive(msg);
 }
 
+template <class Directive>
+inline OtherMessage& operator<<(OtherMessage& msg, const Directive& directive) {
+  return directive(msg);
+}
 
 // Encapsulation - by moving logic into the directive types the target object’s
 // interface can remain rudimentary and even hidden to the user’s immediate
@@ -106,6 +140,9 @@ inline Message& operator<<(Message& msg, const Directive& directive) {
 // applied to a broad class of situations. In a header-only library, the static
 // nature of the wiring and chaining of the operations lends itself to compiler abuse.
 int main() {
+
+    std::cout << "**** Message ****" << std::endl;
+
     Message msg;
     msg << source("me")
         << destination("you")
@@ -114,6 +151,29 @@ int main() {
     std::cout << "message.source: " << msg.source << std::endl;
     std::cout << "message.destination: " << msg.destination << std::endl;
     std::cout << "message.id: " << msg.id << std::endl;
+
+    msg << to_defaults();
+
+    std::cout << "message.source: " << msg.source << std::endl;
+    std::cout << "message.destination: " << msg.destination << std::endl;
+    std::cout << "message.id: " << msg.id << std::endl;
+
+    msg << to_defaults() << destination("you") << id(47ULL);;
+
+    std::cout << "message.source: " << msg.source << std::endl;
+    std::cout << "message.destination: " << msg.destination << std::endl;
+    std::cout << "message.id: " << msg.id << std::endl;
+
+    std::cout << "**** OtherMessage ****" << std::endl;
+
+    OtherMessage otherMsg;
+    otherMsg << source("me")
+        << destination("you")
+        << id(47ULL);
+
+    std::cout << "otherMsg.source: " << otherMsg.source << std::endl;
+    std::cout << "otherMsg.destination: " << otherMsg.destination << std::endl;
+    std::cout << "otherMsg.otherId: " << otherMsg.otherId << std::endl;
 
     return 0;
 }
